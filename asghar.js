60d1601cbdfae6dbf873c5943e5e604a734909cb
muneebabid381 <muneebabid381@gmail.com>
2022-10-07 10:14:29 -0700

    initial commit

---
+++ asghar.js
@@ -0,0 +1,28 @@
+//const alphabet=()=>{
+ 
+/*alert("hello java");
+
+var a = prompt("aja ty beja");
+
+alert(a);
+*/
+/*var a=Math.floor(Math.random()*1000);
+
+console.log("A random number has been generated. Less than 1000. You have three attempts to guess the correct number");
+
+for(var i=0; i<3; i++){
+    var num=prompt("Guess the number");
+    if(num>a){
+        alert("go low");
+    }
+    else if (num<a){
+        alert("go high");
+    }
+    else {alert("Bingo"); break;}
+}
+if(i===3)
+alert("Number was "+a)
+
+   
+}*/
+
